c323c5825a90fb77efd7610ebe697f9fc0b273e7
cyntraten <173311707+cyntraten@users.noreply.github.com>
2026-03-02 16:11:32 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module calculator-rest-echo
+
+go 1.25.7
+
+require (
+	github.com/Knetic/govaluate v3.0.0+incompatible
+	github.com/labstack/echo/v5 v5.0.4
+)
+
+require (
+	github.com/google/uuid v1.6.0
+	golang.org/x/time v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Knetic/govaluate"
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+)
+
+type Calculation struct {
+	ID         string `json:"id"`
+	Expression string `json:"expression"`
+	Result     string `json:"result"`
+}
+
+type CalculationRequest struct {
+	Expression string `json:"expression"`
+}
+
+var calculations = []Calculation{}
+
+func calculateExpression(expression string) (string, error) {
+	expr, err := govaluate.NewEvaluableExpression(expression)
+	if err != nil {
+		return "", err
+	}
+
+	result, err := expr.Evaluate(nil)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%v", result), err
+
+}
+
+func getCalculations(c *echo.Context) error {
+	return c.JSON(http.StatusOK, calculations)
+
+}
+
+func postCalculations(c *echo.Context) error {
+	var req CalculationRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+
+	result, err := calculateExpression(req.Expression)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid expression"})
+	}
+
+	calc := Calculation{
+		ID:         uuid.NewString(),
+		Expression: req.Expression,
+		Result:     result,
+	}
+
+	calculations = append(calculations, calc)
+
+	return c.JSON(http.StatusCreated, calc)
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodPost, http.MethodOptions, http.MethodGet},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
+	e.Use(middleware.RequestLogger())
+
+	e.GET("/calculations", getCalculations)
+	e.POST("/calculations", postCalculations)
+	e.Start("localhost:8080")
+
+}
